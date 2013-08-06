8405745b2b3e2811583971f2821f676411d4ac7d
shuimurenyizu <shuimurenyizu@163.com>
2013-08-06 11:26:00 +0800

    finish user signup

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -34,4 +34,8 @@ Sample::Application.configure do
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
+
+  # Speed up tests by lowering bcrypt's cost function.
+  #ActiveModel::SecurePassword.min_cost = true
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Sample::Application.routes.draw do
-  get "users/new"
+  resources :users
 
   # get "pages/home"
   # get "pages/help"
